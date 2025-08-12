c2184fdf0bdaf4e4e99c11f97a71a252cb0db5dd
Pdammie <ainadammie96@gmail.com>
2025-08-12 19:48:27 +0100

    i have commited this

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { getProfile, updateProfile, deleteProfile } from '../controllers/userController.js';
-import auth from '../middlewares/authMiddleware.js';
+import { getProfile, updateProfile, deleteProfile } from '../controllers/userControllers.js';
+import auth from '../middlewares/authMiddlewares.js';
 
 
 const router = express.Router();
